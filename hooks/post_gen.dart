349aed34cd565736049d997c45682f09237f0e1d
Alexandr <alexandr9038@protonmail.com>
2025-12-06 02:57:08 -0800

    Disabled hooks

--- hooks/post_gen.dart
+++ hooks/post_gen.dart
@@ -3,10 +3,10 @@ import 'dart:io';
 import 'package:mason/mason.dart';
 
 void run(HookContext context) {
-  final flagFile = File(
-    '.mason_initialized',
-  );
-  flagFile.writeAsStringSync(
-    'initialized: true',
-  );
+  // final flagFile = File(
+  //   '.mason_initialized',
+  // );
+  // flagFile.writeAsStringSync(
+  //   'initialized: true',
+  // );
 }
--- hooks/pre_gen.dart
+++ hooks/pre_gen.dart
@@ -5,17 +5,17 @@ import 'package:mason/mason.dart';
 /// https://docs.brickhub.dev/hooks/
 
 void run(HookContext context) {
-  final flagFile = File(
-    '.mason_initialized',
-  );
-
-  if (flagFile.existsSync()) {
-    context.logger.err(
-      'This project has already been initialized.',
-    );
-    context.logger.err(
-      'Remove .mason_initialized if you REALLY want to re-run the template.',
-    );
-    exit(1);
-  }
+  // final flagFile = File(
+  //   '.mason_initialized',
+  // );
+  //
+  // if (flagFile.existsSync()) {
+  //   context.logger.err(
+  //     'This project has already been initialized.',
+  //   );
+  //   context.logger.err(
+  //     'Remove .mason_initialized if you REALLY want to re-run the template.',
+  //   );
+  //   exit(1);
+  // }
 }
